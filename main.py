20c64da9d236da46f325e26b619161018319fa09
Victoria <vmoix001@mail.ru>
2020-03-16 00:48:21 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Case-study №3
+# Developers: Moiseenko V., Vlasov V.
+# The aim: to write a program for performing two-color paving of the plane with regular hexagons
+
+import turtle
+
+# For Victoria:
+def get_color_choice(): # to add colours, take two colours, at least 6 colours, reentry if wrong
+    pass
+
+# For Vlas:
+def get_num_hexagons(N): # to check whether data entry is correct, 4 <= N <= 20
+    pass
+
+# For both:
+def draw_hexagon(x, y):
+    # For Victoria: to count the side of haxagon, to draw the hexagon + code review
+    # For Vlas: to draw the line of hexagons
+    pass
+
+
+turtle.done()
